8e364641f9a3aac1bf1dfbf81aca4e112c96bb14
Yuri Santana <yuri.dev35@gmail.com>
2026-02-28 22:40:33 -0300

    refactor: standardize indentation in User entity class

--- src/modules/user/domain/user.entity.ts
+++ src/modules/user/domain/user.entity.ts
@@ -1,15 +1,15 @@
 // Domain Layer - Pure TypeScript Entity
 export class User {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    admin: boolean;
-    communityId: string;
-    position: string;
-    image?: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  admin: boolean;
+  communityId: string;
+  position: string;
+  image?: string;
 
-    constructor(data: Partial<User>) {
-        Object.assign(this, data);
-    }
+  constructor(data: Partial<User>) {
+    Object.assign(this, data);
+  }
 }
